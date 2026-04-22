5cf0b17afa12eb3cb1b4403acca2a86346bc9791
Eduardo Melo Glaser <eduardomeloglaser@gmail.com>
2026-04-22 19:48:18 -0300

    semana_8

--- PEM-20261-Semana08/semana8EduardoMeloGlaser.c
+++ PEM-20261-Semana08/semana8EduardoMeloGlaser.c
@@ -1,38 +1,42 @@
+///---------------------------------------------------------
+//*                    FATEC Ipiranga                      *
+//* Disciplina: Programaçao Estruturada e Modular          *
+//*          Prof. Veríssimo                               *
+//----------------------------------------------------------
+//* Objetivo é criar um programa que lê o peso de 10       *
+//  produtos, calcular a média do peso e então alterar os  *
+//  valores de acordo com a condição especificada, caso    *
+//  o peso seja maior que a média*1.1 o peso do produto    *
+//  terá um reajuste do peso*0.95, o programa tem como foco*
+//  alterar os valores das variáveis usando ponteiros      *
+//  sempre que der                                         *
+//* Data - 22/04/2026                                      * 
+//* Autor: Eduardo Melo Glaser;2040482522021               *
+///--------------------------------------------------------/
+
+
 #include <stdio.h>
 
-//validar peso de carregamentos
-//sistema recebe um lote de pacotes(vetor)
-
-//gerenciar o peso de uma frota de 10 pacotes 
-//uso de ponteiros 
-//peso deve ser tipo float
-//implementar função:  void processarCarga(float *vetor, int tamanho)
-//  essa função deve calcular a média dor pesos
-//  localizar preços que exceadam a média em mais de 10%
-//  remover 5% do peso via ponteiro  
-//  exibir o resultado antes e depois 
-
-//float peso produtos[10] 
-//float *p = &produtos 
-
-float media = 0;
-int tamanho = 10;
+float media = 0; //define a variavel media
+int tamanho = 10; // define a variavel tamanho
 
 
-void lerPesoProdutos(float *pesoProdutos, int tamanho){
+void lerPesoProdutos(float *pesoProdutos, int tamanho){ // void para ler o valor do peso de todos os produtos
 
 
-    for(int i = 0;i < 10; i++){
+    for(int i = 0;i < tamanho; i++){ // looping for para ler os valores 
 
         printf("PRODUTO %d: ", i+1);
-        scanf("%f" ,&*pesoProdutos+i);
-        media += *(pesoProdutos+i);
+        scanf("%f" ,pesoProdutos+i); //leitor de valores que coloca o valor direto no endereço de memória usando ponteiros
+        media += *(pesoProdutos+i); // calculo da media
     }
-    media = media/tamanho;
+    media = media/tamanho; // calculo de media
 
 };
 
-void processarCarga(float *vetor, int tamanho){
+void processarCarga(float *vetor, int tamanho){ // void para processar os valores lidos e realizar as especificações do exercicio
+
+    // definição de variáveis floats e ponteiros
 
     float valorFinal[10] = { 0 };
     float *pValorFinal = valorFinal;
@@ -40,12 +44,17 @@ void processarCarga(float *vetor, int tamanho){
     mediaAtualizada = &media;
     *mediaAtualizada = *mediaAtualizada*1.1;
 
-    //mostra a lista
+    //mostra a lista antes da alteração
 
     printf("===================================\n               ANTES               \n===================================\n\n\n");           
     for(int i = 0;i < tamanho; i++){
-        printf("PRODUTO %d: %.2f\n", i+1 ,*(vetor+i));
-        if(*(vetor+i) > *mediaAtualizada){
+        printf("PRODUTO %d: %.2f\n", i+1 ,*(vetor+i)); // mostra o numero do produto atual e o peso
+
+        // comando If que verifica se o peso do produto está acima da média*1.1, 
+        //se ele estiver o valor do peso será multiplicado por 0.95 e será guardado no *pValorFinal+1
+        // caso o peso nao seja maior, o valor é guardado na mesma variável, porém, sem alteração
+
+        if(*(vetor+i) > *mediaAtualizada){ 
             *(pValorFinal+i) = (*(vetor+i))*0.95; 
         }
         else{
@@ -53,10 +62,14 @@ void processarCarga(float *vetor, int tamanho){
         }
     }
 
-    printf("\n\nmedia: %.2f\n\n", media);
+    printf("\n\nmedia: %.2f\n\n", media); // mostra o valor da media
+
+    // exibe a lista depois das alterações
 
     printf("===================================\n               DEPOIS               \n===================================\n\n\n");
 
+
+    //for para exibir a lista inteira 
     for(int i = 0;i < tamanho; i++){
         printf("PRODUTO %d: %.2f\n",i+1 ,*(pValorFinal+i));
     }
@@ -66,10 +79,10 @@ void processarCarga(float *vetor, int tamanho){
 
 int main (){
 
-    float pesoProdutos[10] = { 0 };
+    float pesoProdutos[10] = { 0 }; //define a quantidade de produtos, e o valor que por enquanto não esxiste
 
-    lerPesoProdutos(pesoProdutos, tamanho);
-    processarCarga(pesoProdutos, tamanho);
+    lerPesoProdutos(pesoProdutos, tamanho); //chama o void lerPesoProdutos
+    processarCarga(pesoProdutos, tamanho); //chama o void processarCarga
 
     return 0;
 }
